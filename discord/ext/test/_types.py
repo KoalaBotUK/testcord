a6fa99559a888994060ad651af422437c2ad21de
JayDwee <drapj002@gmail.com>
2022-01-23 22:31:02 +0000

    refactor: moved FakeHttp to new file

--- discord/ext/test/_types.py
+++ discord/ext/test/_types.py
@@ -11,4 +11,13 @@ JsonVal = typing.Union[str, int, bool, typing.Dict[str, 'JsonVal'], typing.List[
 JsonDict = typing.Dict[str, JsonVal]
 JsonList = typing.List[JsonVal]
 Callback = typing.Callable[..., typing.Coroutine[None, None, None]]
-AnyChannel = typing.Union[discord.TextChannel, discord.CategoryChannel, discord.abc.GuildChannel, discord.abc.PrivateChannel]
+AnyChannel = typing.Union[discord.TextChannel, discord.CategoryChannel,
+                          discord.abc.GuildChannel, discord.abc.PrivateChannel]
+
+
+class FakeRequest(typing.NamedTuple):
+    """
+        A fake web response, for use with discord ``HTTPException``
+    """
+    status: int
+    reason: str
